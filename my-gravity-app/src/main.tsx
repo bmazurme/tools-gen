43d58a1eee7896cd464b31ddffc228f69a9556c8
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-05-18 11:12:05 +0300

    fix: main

---
+++ my-gravity-app/src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import './index.css'
+// import '@gravity-ui/uikit/styles/styles.css';
+// import '@gravity-ui/aikit/styles.css';
+// import '@gravity-ui/aikit/dist/styles.css';
+import '@gravity-ui/uikit/styles/fonts.css';
+import '@gravity-ui/uikit/styles/styles.css';
+// import '@gravity-ui/aikit/dist/styles.css';
+
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
